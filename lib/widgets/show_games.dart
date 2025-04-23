64c1314aba91a74bfd76e3b05fdcd33bf00192a0
G4brielllo <gabrieltarasiuk3@gmail.com>
2025-04-23 10:44:09 +0200

    update ShowGames widget style

--- lib/widgets/show_games.dart
+++ lib/widgets/show_games.dart
@@ -1,4 +1,3 @@
-import 'package:dartz/dartz_unsafe.dart';
 import 'package:flutter/material.dart';
 import 'package:sportsy_front/custom_colors.dart';
 import 'package:sportsy_front/modules/tournament_services/creation_team_list.dart';
@@ -7,6 +6,7 @@ import 'package:sportsy_front/modules/tournament_services/teams_temporary.dart';
 class ShowGames extends StatefulWidget {
   ShowGames({super.key});
   final List<Team> teamsList = teamsTemporary();
+
   @override
   State<ShowGames> createState() => _ShowGamesState();
 }
@@ -14,30 +14,68 @@ class ShowGames extends StatefulWidget {
 class _ShowGamesState extends State<ShowGames> {
   @override
   Widget build(BuildContext context) {
-    return 
-    
-    Column(
-      children: [
-        for(var team in widget.teamsList)
-        Container(
-          height: 100,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: AppColors.primary,
-            borderRadius: BorderRadius.circular(100),
+    return Container(
+      child: ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: widget.teamsList.length,
+        itemBuilder: (context, index) {
+          final team = widget.teamsList[index];
+          return _buildGameCard(team);
+        },
+      ),
+    );
+  }
+
+  Widget _buildGameCard(Team team) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      decoration: BoxDecoration(
+        color: AppColors.primary,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
           ),
-          child: Row(
-            
-            children: [
-              Expanded(child: Image(image: AssetImage('lib/assets/logo.png'),)),
-              Text('${team.name} vs ${team.name}',),
-        
-              Expanded(child: Image(image: AssetImage('lib/assets/logo.png'),)),
-            
-            ],
+        ],
+      ),
+      child: Row(
+        children: [
+          _buildTeamLogo(),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Text(
+              '${team.name} vs ${team.name}',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.5,
+                shadows: [
+                  Shadow(
+                    offset: Offset(1.5, 1.5),
+                    blurRadius: 3.0,
+                    color: Colors.black38,
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ],
+          const SizedBox(width: 10),
+          _buildTeamLogo(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTeamLogo() {
+    return SizedBox(
+      width: 40,
+      height: 40,
+      child: Image.asset('lib/assets/logo.png'),
     );
   }
 }
